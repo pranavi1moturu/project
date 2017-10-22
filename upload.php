51cbca1c7eb55f7f968ddc15762fe377513cbbb1
pranavi moturu <pm487@afsconnect1.njit.edu>
2017-10-22 14:31:02 -0400

    added code for uploading file to afs

---
+++ upload.php
@@ -0,0 +1,13 @@
+$target_dir = "uploads/";
+$target_file = $target_dir .basename($_FILES["fileToUpload"]["name"]);
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+$uploadOk=1;
+$tmp_name = $_FILES["fileToUpload"]["tmp_name"];
+$name = basename($_FILES["fileToUpload"]["name"]);
+if ($imageFileType != 'csv')
+ {
+ echo 'You can only upload a CSV file';
+ }
+ else
+ {
+move_uploaded_file($tmp_name, "$target_dir/$name");
