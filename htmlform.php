631e4bb08b1985afbc6f8aa2ba8bdf221c815017
pranavi moturu <pm487@afsconnect1.njit.edu>
2017-10-22 13:33:52 -0400

    added html form

---
+++ htmlform.php
@@ -0,0 +1,13 @@
+
+
+<html>
+<body>
+<div align="center">
+<form action="table.php" method="post" enctype="multipart/form-data">
+SELECT FILE: <br>
+<input type="file" name="fileToUpload" id="fileToUpload"> <br> 
+<input type="submit" value="upload File" name="submit">
+</form>
+</div>
+</body>
+</html>
